46522a01a9c6ad99e14589b2b4a20733f80b99b1
jacen11 <jacen19931@gmail.com>
2019-09-05 18:20:00 +0300

    retrofit

---
+++ app/src/main/java/com/pastukhov/top1000currencies/rest/CoinGeckoApi.kt
@@ -0,0 +1,28 @@
+package com.pastukhov.top1000currencies.rest
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface CoinGeckoApi {
+
+    //запрос списка криптовалют
+    @GET("coins/markets")
+    fun getCoinMarket(
+        @Query("vs_currency") vs: String = "usd",
+        @Query("per_page") perPage: Int = 100,
+        @Query("sparkline") sparkline: Boolean = false,
+        @Query("order") order: String = "market_cap_desc"
+    ): Observable<List<GeckoCoin>>
+
+
+    //запрос данных для графика
+    @GET("coins/{id}/market_chart")
+    fun getCoinMarketChart(
+        @Path("id") id: String,
+        @Query("vs_currency") vsCurrency: String = "usd",
+        @Query("days") days: String = "max"
+    ): Observable<GeckoCoinChart>
+
+}
